d4683cc995f8b7d92763d7226b83f9d134006ce8
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-02-14 20:59:04 +1100

    feat: 20200214 correct sort_marchant

--- src/js/HackerRank/sort_marchant.js
+++ src/js/HackerRank/sort_marchant.js
@@ -1,17 +1,18 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
 export function sockMerchant(n, ar) {
-  const sortedArray = ar.sort((a, b) => a - b);
+  let previous = '';
 
-  let previous;
+  return ar
+    .sort((a, b) => a - b)
+    .reduce((count, current) => {
+      if (previous !== current) {
+        previous = current;
+      } else {
+        previous = '';
+        count += 1;
+      }
 
-  return sortedArray.reduce(function(count, current) {
-    if (previous !== current) {
-      previous = current;
-    } else {
-      previous = '';
-    }
-
-    return count;
-  }, 0);
+      return count;
+    }, 0);
 }
