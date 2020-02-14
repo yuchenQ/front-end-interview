46f97a34696fa77406410e5c5a400efa425bb9e6
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-02-14 21:35:59 +1100

    feat: 20200214 rewrite mini swaps 2

--- src/js/HackerRank/minimun_swaps_2.js
+++ src/js/HackerRank/minimun_swaps_2.js
@@ -1,25 +1,20 @@
 // https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
-// 排序过的数组，index, value是一一对应的，如果在原数组里，某个元素处于对的位置，那么它一定不会参与互换
+// 排序过的数组，index, value是一一对应的，而且 value = index + 1, 如果在原数组里，某个元素处于对的位置，那么它一定不会参与互换
 // 所以通过遍历原数组，发现index value对不上就把正确的value换过来，这样会出现：
 // 1.一个元素位置对了
 // 2.两个元素位置都对了
 // 所以遍历完之后，这个数组就被排好序了
 export function minimumSwaps(arr) {
-  const sortedArray = arr.slice().sort((a, b) => a - b);
-  const indexes = new Map();
+  return arr.reduce((swaps, value, index) => {
+    if (value !== index + 1) {
+      // 从 index 之后开始找，是因为 index 之前的元素都排好了，不用找了
+      const rightIndex = arr.indexOf(index + 1, index);
 
-  arr.forEach((v, i) => indexes.set(v, i));
+      [arr[index], arr[rightIndex]] = [arr[rightIndex], arr[index]];
 
-  let swaps = 0;
-
-  arr.forEach((v, i) => {
-    if (v !== sortedArray[i]) {
-      swaps++;
-      arr[indexes.get(sortedArray[i])] = v;
-      arr[i] = sortedArray[i];
-      indexes.set(v, indexes.get(sortedArray[i]));
+      return swaps + 1;
     }
-  });
 
-  return swaps;
+    return swaps;
+  }, 0);
 }
