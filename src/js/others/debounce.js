1b8043d838f10687469ada4c8018d24e77f6143b
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-02-29 16:16:30 +1100

    feat: throttle and debounce

---
+++ src/js/others/debounce.js
@@ -0,0 +1,47 @@
+/* eslint-disable prefer-rest-params */
+// https://github.com/pekonchan/Blog/issues/14
+/**
+ * 防抖函数
+ * @param {Function} fn - 实际要执行的函数
+ * @param {Number} wait - 规定在什么时间内执行一次函数，单位是秒
+ * @param {Boolean} immediate - 是否立即执行，true为立即执行，立即执行指触发监听事件是先执行
+ * @return {Function} 经过防抖处理后的要执行的函数
+ */
+export function debounce(fn, wait = 500, immediate = true) {
+  let timerId = null;
+
+  if (typeof fn !== 'function') {
+    throw new Error('Param Error in debounce(): The first param should be a function!');
+  }
+
+  // 防抖后的执行函数
+  function doing() {
+    if (timerId) clearTimeout(timerId);
+    // 如果是立即执行
+    if (immediate) {
+      // 如果已经过了规定时间，则执行函数 或 第一次触发监听事件
+      fn.apply(this, arguments);
+      // 规定时间后情况定时器id，表明到达了规定时间
+      timerId = setTimeout(() => {
+        timerId = null;
+      }, wait);
+    } else {
+      // 延后执行
+      // 只有到达了规定时间后才会执行fn函数
+      timerId = setTimeout(() => {
+        fn.apply(this, arguments);
+        timerId = null;
+      }, wait);
+    }
+  }
+
+  // 手动取消该次设定的防抖时间，取消后当成是“第一次触发”一样
+  function cancel() {
+    clearTimeout(timerId);
+    timerId = null;
+  }
+
+  doing.cancel = cancel;
+
+  return doing;
+}
---
+++ src/js/others/throttle.js
@@ -0,0 +1,58 @@
+/* eslint-disable prefer-rest-params */
+/**
+ * 节流函数
+ * @param {Function} fn - 实际要执行的函数，对其进行节流处理
+ * @param {Number} wait - 规定的执行时间间隔
+ * @param {Object} option - 用于设置节流的函数的触发时机，
+ *                        - 默认是{leading: true, trailing: true}，表示第一次触发监听事件马上执行，停止后最后也执行一次
+ *                        - leading为false时，表示第一次触发不马上执行
+ *                        - trailing为false时，表示最后停止触发后不执行
+ * @return {Function} 返回经过节流处理后的函数
+ */
+export function throttle(fn, wait = 500, option = {}) {
+  let timerId = null; // 用于记录定时器的id
+  let lastTime = 0; // 上次触发fn的时间戳
+
+  if (typeof fn !== 'function') {
+    throw new Error('Param Error in throttle(): The first param should be a function!');
+  }
+
+  if (option.leading === false && option.trailing === false) {
+    throw new Error("Param Error in throttle(): Cannot set both 'leading' and 'trailing' to true");
+  }
+  // 节流后的执行函数
+  function throttled() {
+    const now = +new Date();
+    // 如果没有上次触发执行时间（即第一次运行），以及leading设置为false
+    if (!lastTime && !option.leading) lastTime = now;
+
+    const remainingTime = wait - (now - lastTime);
+
+    if (remainingTime <= 0 || remainingTime > wait) {
+      // 条件①：如果到达了规定的间隔时间或用户自己设定了系统时间导致的不合理时间差，则立刻执行一次触发函数
+      fn.apply(this, arguments);
+      lastTime = now;
+
+      if (timerId) {
+        clearTimeout(timerId);
+        timerId = null;
+      }
+    } else if (!timerId && option.trailing) {
+      // 条件②：如果未达到规定时间，以及要求停止后延迟执行
+      timerId = setTimeout(() => {
+        fn.apply(this, arguments);
+        timerId = null;
+
+        lastTime = option.leading ? +new Date() : 0;
+      }, remainingTime);
+    }
+  }
+  // 手动提前终止节流时间，恢复初始状态
+  function cancel() {
+    clearTimeout(timerId);
+    timerId = null;
+    lastTime = 0;
+  }
+  throttled.cancel = cancel;
+  return throttled;
+}
